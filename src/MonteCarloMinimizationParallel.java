94ece25e79b2278c8d7317e89e16b6d393fd2200
Msimamisi Lushaba <lushabamsimamisi@gmail.com>
2023-08-12 23:38:47 +0200

    Created Parallel Program structure

---
+++ src/MonteCarloMinimizationParallel.java
@@ -0,0 +1,21 @@
+package MonteCarloMini;
+
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.ForkJoinPool;
+import java.util.Random;
+
+class MonteCarloMinimizationParallel{
+    public static void main (String[] args){
+
+    }
+
+}
+
+
+class Multithreader extends RecursiveAction {
+
+    @Override
+    protected void compute() {
+
+    }
+}
