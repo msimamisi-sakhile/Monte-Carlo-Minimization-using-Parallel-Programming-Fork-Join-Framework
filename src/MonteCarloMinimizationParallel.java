5f32c28105a0f93fd256643da4e87b408a2699d2
Msimamisi S Lushaba <86782399+msimamisi-sakhile@users.noreply.github.com>
2023-08-13 22:55:10 +0200

    Fixed parallel program issue

--- src/MonteCarloMinimizationParallel.java
+++ src/MonteCarloMinimizationParallel.java
@@ -1,10 +1,11 @@
 package MonteCarloMini;
 
-import java.util.concurrent.RecursiveAction;
-import java.util.concurrent.ForkJoinPool;
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
-class MonteCarloMinimizationParallel{
+class MonteCarloMinimizationParallel {
+
     static final boolean DEBUG=false;
 
     static long startTime = 0;
@@ -18,29 +19,28 @@ class MonteCarloMinimizationParallel{
         endTime=System.currentTimeMillis();
     }
 
-    public static void main (String[] args){
-
+    public static void main(String[] args) {
         int rows, columns; //grid size
         double xmin, xmax, ymin, ymax; //x and y terrain limits
         TerrainArea terrain;  //object to store the heights and grid points visited by searches
         double searches_density;	// Density - number of Monte Carlo  searches per grid position - usually less than 1!
-
+        int min;
         int num_searches;		// Number of searches
         Search [] searches;		// Array of searches
         Random rand = new Random();  //the random number generator
-
+        /*
         if (args.length!=7) {
             System.out.println("Incorrect number of command line arguments provided.");
             System.exit(0);
-        }
+        }*/
         /* Read argument values */
-        rows =Integer.parseInt( args[0] );
-        columns = Integer.parseInt( args[1] );
-        xmin = Double.parseDouble(args[2] );
-        xmax = Double.parseDouble(args[3] );
-        ymin = Double.parseDouble(args[4] );
-        ymax = Double.parseDouble(args[5] );
-        searches_density = Double.parseDouble(args[6] );
+        rows =Integer.parseInt( "10" );
+        columns = Integer.parseInt( "10" );
+        xmin = Double.parseDouble("1" );
+        xmax = Double.parseDouble("10000" );
+        ymin = Double.parseDouble("1" );
+        ymax = Double.parseDouble("10000" );
+        searches_density = Double.parseDouble("50000");
 
         if(DEBUG) {
             /* Print arguments */
@@ -63,21 +63,96 @@ class MonteCarloMinimizationParallel{
             //terrain.print_heights();
         }
 
-    }
+        //all searches
+        int local_min=Integer.MAX_VALUE;
+        int finder =-1;
 
+
+        //start timer
+        tick();
+        //---------------------------------START PARALLEL EXECUTION------------------------------------------------------------------//
+
+        try{
+            ForkJoinPool searchThreadPool = new ForkJoinPool();
+            SearchMultithreader parallelSearchTask = new SearchMultithreader(searches, 0,num_searches,false);
+            finder = parallelSearchTask.finder;
+            min = searchThreadPool.invoke(parallelSearchTask);
+        }
+        catch (Exception e){
+            System.out.println("Check Multithreader");
+            min = 0;
+        }
+        //---------------------------------STOP PARALLEL EXECUTION------------------------------------------------------------------//
+        //end timer
+        tock();
+
+        if(DEBUG) {
+            /* print final state */
+            terrain.print_heights();
+            terrain.print_visited();
+        }
+
+        System.out.printf("Run parameters\n");
+        System.out.printf("\t Rows: %d, Columns: %d\n", rows, columns);
+        System.out.printf("\t x: [%f, %f], y: [%f, %f]\n", xmin, xmax, ymin, ymax );
+        System.out.printf("\t Search density: %f (%d searches)\n", searches_density,num_searches );
+
+        /*  Total computation time */
+        System.out.printf("Time: %d ms\n",endTime - startTime );
+        int tmp=terrain.getGrid_points_visited();
+        System.out.printf("Grid points visited: %d  (%2.0f%s)\n",tmp,(tmp/(rows*columns*1.0))*100.0, "%");
+        tmp=terrain.getGrid_points_evaluated();
+        System.out.printf("Grid points evaluated: %d  (%2.0f%s)\n",tmp,(tmp/(rows*columns*1.0))*100.0, "%");
+
+        /* Results*/
+        System.out.printf("Global minimum: %d at x=%.1f y=%.1f\n\n", min, terrain.getXcoord(searches[finder].getPos_row()), terrain.getYcoord(searches[finder].getPos_col()) );
+
+    }
 }
 
+class SearchMultithreader extends RecursiveTask<Integer> {
 
+    Search [] searches;
+    int startPoint, endPoint, finder;
+    boolean DEBUG;
+    int min;
+    private static final int SERIAL_CUTOFF = 551000;
 
-class Multithreader extends RecursiveAction {
-
-    public Multithreader(){
-
+    public SearchMultithreader(Search [] searches, int startPoint, int endPoint, boolean DEBUG) {
+        this.searches = searches;
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.DEBUG = DEBUG;
+        this.min = Integer.MAX_VALUE;
+        this.finder = startPoint;
     }
 
     @Override
-    protected void compute() {
+    protected Integer compute() {
+        int searchWidth = endPoint - startPoint;
+        if (searchWidth < SERIAL_CUTOFF){
+            for  (int i=startPoint;i<endPoint;i++) {
+                int local_min=searches[i].find_valleys();
+                if((!searches[i].isStopped())&&(local_min<min)) { //don't look at  those who stopped because hit existing path
+                    min=local_min;
+                    //finder=i; //keep track of who found it
+                }
+                if(DEBUG) System.out.println("Search "+searches[i].getID()+" finished at  "+local_min + " in " +searches[i].getSteps());
+            }
 
+            return min;
+        }
+        else{
+            int midPoint = (startPoint+endPoint)/2;
+            SearchMultithreader leftFork = new SearchMultithreader(searches, startPoint, midPoint, false);
+            SearchMultithreader rightFork = new SearchMultithreader(searches, midPoint, endPoint, false);
 
+            leftFork.fork();
+            int rightSearchResults = rightFork.compute();
+            int leftSearchResults = leftFork.join();
+
+            int finalMinimunmVal = Math.min(rightSearchResults, leftSearchResults);
+            return finalMinimunmVal; // Minimum Value Output
+        }
     }
 }
